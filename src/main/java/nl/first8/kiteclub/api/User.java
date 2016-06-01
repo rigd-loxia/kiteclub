cae3cf180cffcb87112c9b67a2e908cff7944d1e
Mark Muizer <mark.muizer@rigd-loxia.nl>
2016-06-01 10:44:36 +0200

    wijzigingen tbv poc

--- src/main/java/nl/first8/kiteclub/api/User.java
+++ src/main/java/nl/first8/kiteclub/api/User.java
@@ -33,107 +33,105 @@ import org.keycloak.representations.AccessToken;
 @Stateless
 @Path("/user")
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static final Logger LOG = Logger.getLogger(User.class.getName());
+    private static final Logger LOG = Logger.getLogger(User.class.getName());
 
-	private static final String ATTR_NICKNAME = "nickname";
-	private static final String ATTR_AKA = "aka";
+    private static final String ATTR_NICKNAME = "nickname";
+    private static final String ATTR_AKA = "aka";
 
-	@Resource
-	private SessionContext ctx;
+    @Resource
+    private SessionContext ctx;
 
-	@Context
-	private HttpServletRequest servletRequest;
+    @Context
+    private HttpServletRequest servletRequest;
 
-	@GET
-	@RolesAllowed({ "Member" })
-	@Produces("application/json")
-	public Response get(@Context ServletContext servletContext) {
-		return Response.status(Status.OK).entity(getSessionInfo(servletContext)).build();
-	}
+    @GET
+    @RolesAllowed({"Member"})
+    @Produces("application/json")
+    public Response get(@Context ServletContext servletContext) {
+        return Response.status(Status.OK).entity(getSessionInfo(servletContext)).build();
+    }
 
-	private SessionInfo getSessionInfo(ServletContext servletContext) {
-		@SuppressWarnings("unchecked")
-		KeycloakPrincipal<KeycloakSecurityContext> p = (KeycloakPrincipal<KeycloakSecurityContext>) ctx
-				.getCallerPrincipal();
-		AccessToken token = p.getKeycloakSecurityContext().getToken();
-		// TODO: add user info
-		SessionInfo sessionInfo = new SessionInfo(//
-				servletRequest.getContextPath(), //
-				null, // fullName
-				null, // nickname
-				getAccountUrl(servletContext));
-		return sessionInfo;
-	}
+    private SessionInfo getSessionInfo(ServletContext servletContext) {
+        @SuppressWarnings("unchecked")
+        KeycloakPrincipal<KeycloakSecurityContext> p = (KeycloakPrincipal<KeycloakSecurityContext>) ctx.getCallerPrincipal();
+        AccessToken token = p.getKeycloakSecurityContext().getToken();
+        // TODO: add user info
+        SessionInfo sessionInfo = new SessionInfo(//
+                        servletRequest.getContextPath(), //
+                        null, // fullName
+                        null, // nickname
+                        getAccountUrl(servletContext));
+        return sessionInfo;
+    }
 
-	/**
-	 * Decode JSON Web Token and write to standard out (for debugging purposes).
-	 *
-	 * @param token
-	 */
-	protected void dumpJSONWebToken(String token) {
-		if (token == null || token.isEmpty())
-			return;
+    /**
+     * Decode JSON Web Token and write to standard out (for debugging purposes).
+     *
+     * @param token
+     */
+    protected void dumpJSONWebToken(String token) {
+        if (token == null || token.isEmpty())
+            return;
 
-		System.out.println("***** Dumping JSON Web Token *****");
-		String[] jwt = token.split("\\.");
+        System.out.println("***** Dumping JSON Web Token *****");
+        String[] jwt = token.split("\\.");
 
-		assert jwt.length == 3;
+        assert jwt.length == 3;
 
-		try {
-			System.out.println("Header: " + new String(Base64.getDecoder().decode(jwt[0]), "UTF-8"));
-			System.out.println("Body: " + new String(Base64.getDecoder().decode(jwt[1]), "UTF-8"));
-			System.out.println("Signature: " + jwt[2]);
+        try {
+            System.out.println("Header: " + new String(Base64.getDecoder().decode(jwt[0]), "UTF-8"));
+            System.out.println("Body: " + new String(Base64.getDecoder().decode(jwt[1]), "UTF-8"));
+            System.out.println("Signature: " + jwt[2]);
 
-		} catch (UnsupportedEncodingException e) {
-			System.out.println("Error decoding token: " + e.getMessage());
-		}
-	}
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Error decoding token: " + e.getMessage());
+        }
+    }
 
-	@GET
-	@PermitAll
-	@Path("/logout")
-	public Response logout(@Context ServletContext servletContext) {
-		try {
-			servletRequest.logout();
-			HttpSession session = servletRequest.getSession(false);
-			if (session != null) {
-				session.invalidate();
-			}
-			return Response.temporaryRedirect(new URI(getTargetUri(servletContext))).build();
-		} catch (URISyntaxException | ServletException e) {
-			LOG.log(Level.WARNING, "Could not logout", e);
-		}
-		return Response.status(Status.BAD_REQUEST).build();
-	}
+    @GET
+    @PermitAll
+    @Path("/logout")
+    public Response logout(@Context ServletContext servletContext) {
+        try {
+            servletRequest.logout();
+            HttpSession session = servletRequest.getSession(false);
+            if (session != null) {
+                session.invalidate();
+            }
+            return Response.temporaryRedirect(new URI(getTargetUri(servletContext))).build();
+        } catch (URISyntaxException | ServletException e) {
+            LOG.log(Level.WARNING, "Could not logout", e);
+        }
+        return Response.status(Status.BAD_REQUEST).build();
+    }
 
-	public String getAccountUrl(@Context ServletContext servletContext) {
-		String targetUri = getTargetUri(servletContext);
+    public String getAccountUrl(@Context ServletContext servletContext) {
+        String targetUri = getTargetUri(servletContext);
 
-		KeycloakDeployment deployment = getDeployment(servletContext);
+        KeycloakDeployment deployment = getDeployment(servletContext);
 
-		return String.format(
-				"%s/realms/%s/account/" //
-						+ "?referrer=%s", //
-				deployment.getAuthServerBaseUrl(), //
-				deployment.getRealm(), //
-				targetUri);
-	}
+        return String.format(
+                        "%s/realms/%s/account/" //
+                                        + "?referrer=%s", //
+                        deployment.getAuthServerBaseUrl(), //
+                        deployment.getRealm(), //
+                        targetUri);
+    }
 
-	private String getTargetUri(ServletContext servletContext) {
-		// TODO: find out where this can be set...
-		String targetUri = servletContext.getInitParameter("kiteclub.base.url");
-		if (targetUri != null) {
-			return targetUri;
-		}
-		return "http://localhost:8580/kiteclub";
-	}
+    private String getTargetUri(ServletContext servletContext) {
+        // TODO: find out where this can be set...
+        String targetUri = servletContext.getInitParameter("kiteclub.base.url");
+        if (targetUri != null) {
+            return targetUri;
+        }
+        return "http://localhost/kiteclub";
+    }
 
-	private KeycloakDeployment getDeployment(ServletContext servletContext) {
-		AdapterDeploymentContext deploymentContext = (AdapterDeploymentContext) servletContext
-				.getAttribute(AdapterDeploymentContext.class.getName());
+    private KeycloakDeployment getDeployment(ServletContext servletContext) {
+        AdapterDeploymentContext deploymentContext = (AdapterDeploymentContext) servletContext.getAttribute(AdapterDeploymentContext.class.getName());
 
-		return deploymentContext.resolveDeployment(null);
-	}
+        return deploymentContext.resolveDeployment(null);
+    }
 }
